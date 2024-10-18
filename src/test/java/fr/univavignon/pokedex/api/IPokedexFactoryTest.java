a677f023e483a7be9d1e66dc681c1338460e7214
Axel <axel-bertrand@hotmail.fr>
2024-10-18 17:17:49 +0200

    PokedexFactory test

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -0,0 +1,24 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+public class IPokedexFactoryTest {
+	static IPokedexFactory pokedexFactory;
+
+	@BeforeAll
+	static void setPokedexFactory() {
+		pokedexFactory = mock(IPokedexFactory.class);
+
+		when(pokedexFactory.createPokedex(any(), any())).thenReturn(mock(IPokedex.class));
+	}
+
+	@Test
+	void returnNotNullTest() {
+		IPokedex pokedex = pokedexFactory.createPokedex(mock(IPokemonMetadataProvider.class), mock(IPokemonFactory.class));
+		assertNotNull(pokedex);
+	}
+}
