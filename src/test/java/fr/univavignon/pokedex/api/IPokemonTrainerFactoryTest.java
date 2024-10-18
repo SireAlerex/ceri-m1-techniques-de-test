50c76af86908ae6656b2d25cb644d20b5411197a
Axel <axel-bertrand@hotmail.fr>
2024-10-18 17:17:59 +0200

    PokedexTrainerFactory test

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -0,0 +1,25 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+public class IPokemonTrainerFactoryTest {
+	static IPokemonTrainerFactory pokemonTrainerFactory;
+
+	@BeforeAll
+	static void setPokemonTrainerFactory() {
+		pokemonTrainerFactory = mock(IPokemonTrainerFactory.class);
+
+		when(pokemonTrainerFactory.createTrainer(anyString(), any(), any())).thenReturn(mock(PokemonTrainer.class));
+	}
+
+	@Test
+	void returnNotNullTest() {
+		PokemonTrainer trainer = pokemonTrainerFactory.createTrainer("Sacha", Team.VALOR, mock(IPokedexFactory.class));
+		assertNotNull(trainer);
+	}
+}
