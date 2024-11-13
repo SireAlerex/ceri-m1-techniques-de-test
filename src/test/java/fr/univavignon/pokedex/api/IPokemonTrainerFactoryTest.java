e63bdff6dce4c84b9083ea49f12ea3518a7a9191
Axel <axel-bertrand@hotmail.fr>
2024-11-13 10:16:18 +0100

    jacoco

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -3,7 +3,6 @@ package fr.univavignon.pokedex.api;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
